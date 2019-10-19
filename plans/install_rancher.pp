899081917743a1b5946e8c04939b8a97a453cdfe
Paul Talbot <prolixalias@gmail.com>
2019-10-19 16:23:23 -0400

    commented out joins

--- plans/install_rancher.pp
+++ plans/install_rancher.pp
@@ -13,7 +13,7 @@ plan rancher::install_rancher(
   $api_token = $api_token_result_set.first().message
   out::message("*** api_token: ${api_token} ***")
 
-  $set_url_result_set = run_task('rancher::set_rancher_url', 'localhost', control_plane => $control_plane, rancher_url => 'rancher.fervid.us', api_token => $api_token)
+  $set_url_result_set = run_task('rancher::set_rancher_url', 'localhost', control_plane => $control_plane, rancher_url => $control_plane, api_token => $api_token)
 
   $create_cluster_result_set = run_task('rancher::create_cluster', 'localhost', control_plane => $control_plane, cluster_name => $cluster_name, api_token => $api_token)
   $cluster_id = $create_cluster_result_set.first().message
@@ -23,12 +23,12 @@ plan rancher::install_rancher(
   $cluster_join_command = $cluster_join_result_set.first().message
   out::message("*** cluster_join_command: ${cluster_join_command} ***")
 
-  $server_join_command_result_set = run_task('exec', $control_plane, command => "${cluster_join_command} --etcd --controlplane")
-  $server_join_status = $server_join_command_result_set.first().message
-  out::message("*** server_join_status: ${server_join_status} ***")
+  #$server_join_command_result_set = run_task('exec', $control_plane, command => "${cluster_join_command} --etcd --controlplane")
+  #$server_join_status = $server_join_command_result_set.first().message
+  #out::message("*** server_join_status: ${server_join_status} ***")
 
-  $worker_join_command_result_set = run_task('exec', $workers, command => "${cluster_join_command} --worker")
-  $worker_join_status = $worker_join_command_result_set.first().message
-  out::message("*** worker_join_status: ${worker_join_status} ***")
+  #$worker_join_command_result_set = run_task('exec', $workers, command => "${cluster_join_command} --worker")
+  #$worker_join_status = $worker_join_command_result_set.first().message
+  #out::message("*** worker_join_status: ${worker_join_status} ***")
 
 }
