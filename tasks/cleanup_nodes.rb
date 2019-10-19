99b25eb86112a2c33d6a7ac88792cee1b6c32589
Paul Talbot <prolixalias@gmail.com>
2019-10-19 16:46:15 -0400

    absolute path to binaries in cleanup

--- tasks/cleanup_nodes.rb
+++ tasks/cleanup_nodes.rb
@@ -7,13 +7,13 @@ require_relative '../../ruby_task_helper/files/task_helper.rb'
 
 class MyTask < TaskHelper
   def task(name: nil, **_kwargs)
-    cmd_containers_stop = 'sudo docker ps -a | awk \'{ print $1 }\' grep -v ^CON | while read container; do sudo docker stop ${container}; done'
+    cmd_containers_stop = '/usr/bin/sudo /usr/bin/docker ps -a | /usr/bin/awk \'{ print $1 }\' | /usr/bin/grep -v ^CON | while read container; do /usr/bin/sudo /usr/bin/docker stop ${container}; done'
     stdout, stderr, status = Open3.capture3(*cmd_containers_stop) # rubocop:disable Lint/UselessAssignment
 
-    cmd_containers_rm = 'sudo docker ps -a | awk \'{ print $1 }\' grep -v ^CON | while read container; do sudo docker rm ${container}; done'
+    cmd_containers_rm = '/usr/bin/sudo /usr/bin/docker ps -a | /usr/bin/awk \'{ print $1 }\' | /usr/bin/grep -v ^CON | while read container; do /usr/bin/sudo /usr/bin/docker rm ${container}; done'
     stdout, stderr, status = Open3.capture3(*cmd_containers_rm) # rubocop:disable Lint/UselessAssignment
 
-    cmd_images_rm = 'sudo docker image ls | awk \'{ print $3 }\' grep -v ^IMA | while read image; do sudo docker rmi ${image}; done'
+    cmd_images_rm = '/usr/bin/sudo /usr/bin/docker image ls | /usr/bin/awk \'{ print $3 }\' | /usr/bin/grep -v ^IMA | while read image; do /usr/bin/sudo /usr/bin/docker rmi ${image}; done'
     stdout, stderr, status = Open3.capture3(*cmd_images_rm) # rubocop:disable Lint/UselessAssignment
 
     # raise Puppet::Error, _("stderr: ' #{stderr}') % { stderr: stderr }") if status != 0
