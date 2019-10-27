563886b7ea1a87252cb62a94b68fa92a7ead08ee
Paul Talbot <prolixalias@gmail.com>
2019-10-27 15:21:50 -0400

    MIssing current password param

--- plans/change_admin_password.pp
+++ plans/change_admin_password.pp
@@ -1,13 +1,15 @@
 plan rancher::create_cluster_custom(
   TargetSpec $rancher_server,
+  String[1] $current_password = 'admin',
+  String[1] $new_password,
 ) {
 
   $login_token_result_set = run_task('rancher::get_login_token', 'localhost', rancher_server => $rancher_server)
   $login_token = $login_token_result_set.first().message
   out::message("*** login_token: ${login_token}")
 
-  $set_admin_password = run_task('rancher::set_admin_password', 'localhost', rancher_server => $rancher_server, login_token => $login_token, new_password => $new_password)
-  $cluster_id = $create_cluster_result_set.first().message
-  out::message("*** cluster_id: ${cluster_id}")
+  $set_admin_password_resault_set = run_task('rancher::set_admin_password', 'localhost', rancher_server => $rancher_server, login_token => $login_token, current_password => $current_password, new_password => $new_password)
+  $set_admin_password_result = $set_admin_password_result_set.first().message
+  out::message("*** set_admin_password_result: ${set_admin_password_result}")
 
 }
