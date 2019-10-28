02a0b11361c8d2fe7a2daa9276e8ec15d612c9df
Paul Talbot <prolixalias@gmail.com>
2019-10-27 23:14:57 -0400

    typo in variable name

--- plans/change_admin_password.pp
+++ plans/change_admin_password.pp
@@ -8,7 +8,7 @@ plan rancher::change_admin_password(
   $login_token = $login_token_result_set.first().message
   out::message("*** login_token: ${login_token}")
 
-  $set_admin_password_resault_set = run_task('rancher::set_admin_password', 'localhost', rancher_server => $rancher_server, login_token => $login_token, current_password => $current_password, new_password => $new_password)
+  $set_admin_password_result_set = run_task('rancher::set_admin_password', 'localhost', rancher_server => $rancher_server, login_token => $login_token, current_password => $current_password, new_password => $new_password)
   $set_admin_password_result = $set_admin_password_result_set.first().message
   out::message("*** set_admin_password_result: ${set_admin_password_result}")
 
