196de9bcba89de2b4a10cafff41233818420dd13
Paul Talbot <prolixalias@gmail.com>
2019-10-27 23:13:30 -0400

    missing param

--- plans/change_admin_password.pp
+++ plans/change_admin_password.pp
@@ -4,7 +4,7 @@ plan rancher::change_admin_password(
   String[1] $new_password,
 ) {
 
-  $login_token_result_set = run_task('rancher::get_login_token', 'localhost', rancher_server => $rancher_server)
+  $login_token_result_set = run_task('rancher::get_login_token', 'localhost', rancher_server => $rancher_server, admin_password => $current_password)
   $login_token = $login_token_result_set.first().message
   out::message("*** login_token: ${login_token}")
 
