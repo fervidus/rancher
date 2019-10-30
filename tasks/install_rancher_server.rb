c2267a2393c57b4f70d6501245f69b77c766cf54
Paul Talbot <prolixalias@gmail.com>
2019-10-30 18:44:31 -0400

    moved name attribute

--- tasks/install_rancher_server.rb
+++ tasks/install_rancher_server.rb
@@ -7,7 +7,7 @@ require_relative '../../ruby_task_helper/files/task_helper.rb'
 
 class MyTask < TaskHelper
   def task(name: nil, **_kwargs)
-    cmd = '/bin/sudo /bin/docker run -d --mount source=rancher,target=/var/lib/rancher --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher --name rancher'
+    cmd = '/bin/sudo /bin/docker run -d --name rancher --mount source=rancher,target=/var/lib/rancher --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher'
     stdout, stderr, status = Open3.capture3(*cmd) # rubocop:disable Lint/UselessAssignment
     { stdout: stdout.strip.to_json }
   end
