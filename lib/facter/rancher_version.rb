84f939692992553c0fb92eec45abca93d5128390
Paul Talbot <prolixalias@gmail.com>
2019-11-10 19:31:37 -0500

    typo in fact name

--- lib/facter/rancher_version.rb
+++ lib/facter/rancher_version.rb
@@ -4,7 +4,7 @@ Facter.add(:rancher_version) do
     rancher_version = nil
 
     if Facter::Util::Resolution.which('docker')
-      apache_version = Facter::Util::Resolution.exec('docker inspect --format=\'{{println .Config.Image}}\' share-mnt | awk -F: \'{ print $2 }\' 2>&1')
+      rancher_version = Facter::Util::Resolution.exec('docker inspect --format=\'{{println .Config.Image}}\' share-mnt | awk -F: \'{ print $2 }\' 2>&1')
       Facter.debug "Matching image: '#{rancher_version}'"
     end
 
