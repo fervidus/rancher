c447b3f32809ef9f3da3352be23034b38297ec51
Paul Talbot <prolixalias@gmail.com>
2019-11-11 12:13:15 -0500

    Bryan changed

--- lib/facter/rancher_version.rb
+++ lib/facter/rancher_version.rb
@@ -8,11 +8,11 @@ Facter.add(:rancher_version) do
       Facter.debug "Matching image: '#{rancher_version}'"
     end
 
-    unless rancher_version.nil?
-      match = %r{^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$}.match(rancher_version)
-      unless match.nil?
-        match[1]
-      end
-    end
+    rancher_version unless rancher_version.nil?
+    #  match = %r{^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$}.match(rancher_version)
+    #  unless match.nil?
+    #    match[1]
+    #  end
+    # end
   end
 end
