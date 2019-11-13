9fde44d1dd24b011330ac03e618e90d0abacac07
Paul Talbot <prolixalias@gmail.com>
2019-11-12 21:44:18 -0500

    testing rancher_version

--- lib/facter/rancher_version.rb
+++ lib/facter/rancher_version.rb
@@ -5,12 +5,13 @@ Facter.add(:rancher_version) do
 
     if Facter::Util::Resolution.which('docker')
       rancher_node_version = Facter::Util::Resolution.exec('docker inspect --format=\'{{println .Config.Image}}\' share-mnt | awk -F: \'{ print $2 }\' 2>&1')
-      rancher_server_env = Facter::Util::Resolution.exec('docker inspect --format=\'{{println .Config.Env}}\' rancher\' 2>&1')
-      puts rancher_server_env
+      rancher_server_env = Facter::Util::Resolution.exec('docker inspect --format=\'{{println .Config.Env}}\' rancher 2>&1')
+      rancher_server_version = rancher_server_env[/.*CATTLE_AGENT_IMAGE=rancher\/rancher-agent:(v[\d\.]+).*/, 1]
       Facter.debug "Matching image: '#{rancher_node_version}'"
     end
 
-    rancher_version unless rancher_version.nil?
+    rancher_node_version unless rancher_node_version.nil?
+    rancher_server_version unless rancher_server_version.nil?
     #  match = %r{^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$}.match(rancher_version)
     #  unless match.nil?
     #    match[1]
